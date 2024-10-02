c5da83f20e8346483f5aea2e4bb0781fe15b3bf6
Lana Siqueira <131266569+helanasiqueira2023@users.noreply.github.com>
2024-10-02 20:31:09 -0300

    Add files via upload
    exercício 03 : 02/10

---
+++ ex3 - PPO.py
@@ -0,0 +1,26 @@
+﻿numero1 = 10
+
+numero2 = 8
+
+print(numero1 + numero2)
+
+---------------------------
+primeironome = "Helana"
+sobrenomedousuario = " dos Santos de Siqueira"
+
+print('Seu nome completo é: ', primeironome + sobrenomedousuario)
+
+---------------------------
+
+nota1 = 8 
+nota2 = 4 
+nota3 = 2 
+
+media = print('Sua média é: ', nota1 + nota2 + nota3 / 3)
+---------------------------------------------------------------------------------------
+
+largura = 8 
+altura = 4
+
+
+área = print('A área de um retângulo é: ', largura * altura)
