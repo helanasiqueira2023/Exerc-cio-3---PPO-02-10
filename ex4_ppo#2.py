0e5ccab75ff80ed92b5afd2289cbc3f48445af54
Lana Siqueira <131266569+helanasiqueira2023@users.noreply.github.com>
2024-10-04 20:47:40 -0300

    Add files via upload
    EXERCÍCIO 4: => 2 & 3

---
+++ ex4_ppo#2.py
@@ -0,0 +1,7 @@
+#EXERCÍCIO 4 ///////////////////////////////////////////////////////////////////////
+
+#2:
+horas = int(input("Informe uma quantidade em horas:"))
+minutos = horas*60
+segundos = minutos*60 
+print("Conversor de horas pra minutos é", minutos, "e horas para segundos é", segundos)
---
+++ ex4_ppo#3.py
@@ -0,0 +1,10 @@
+nome = input("Informe seu nome:")
+sobrenome = input("Informe também seu sobrenome:")
+idade = input("Informe agora sua idade:")
+cidade = input("Informe a cidade de onde você mora:")
+estado = input("Estado que você mora:")
+email = input("Informe o seu email:")
+telefone = input("Agora por último seu telefone:")
+ano = 2024 - idade
+
+print("Seu nome é:", nome, "nascimento no ano de", ano, "da cidade de:", cidade,"estado de:", estado, "com portador do telefone:", telefone, "com principal e-mail:", email, " foi realizado cadastro com sucesso.")
