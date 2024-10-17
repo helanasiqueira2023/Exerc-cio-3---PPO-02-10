89618c64e83cd4f52ec8002bb9bc20abe13aaf52
Lana Siqueira <131266569+helanasiqueira2023@users.noreply.github.com>
2024-10-16 21:20:10 -0300

    Add files via upload
    Exercício 5 = 16/10

---
+++ ex5-1).py
@@ -0,0 +1,8 @@
+idade = float(input('Informe sua idade:'))
+
+if idade >= 18: 
+    print('Você é de maior!!')
+else: idade < 18
+print(f'Você ainda é de menor!!')
+
+    
---
+++ ex5-2).py
@@ -0,0 +1,14 @@
+num1 = float(input('Informeo o 1° número aleatório:'))
+num2 = float(input('Informe agora o 2° número aleatório:'))
+
+if num1 > num2:
+    print('O número 1 é maior que o número 2.')
+else:
+    print('O número 1 é menor que o número 2.')
+
+if num1 == num2:
+    print('Os números são iguais.')
+else:
+    print('Os números são diferentes.')
+
+
