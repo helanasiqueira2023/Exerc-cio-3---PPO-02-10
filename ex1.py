efc3b46d215b0b68679dc3162ea5dd5564067b28
Lana Siqueira <131266569+helanasiqueira2023@users.noreply.github.com>
2024-10-25 21:44:49 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,15 @@
+class Carro ():
+    def __init__(self, marca, ano, cor):
+        self.marca = marca
+        self.ano = ano
+        self.cor = cor
+
+#entrada.....
+marca_inp = input("Digite a marca de seu carro:")
+ano_inp = input("Digite o ano de seu carro:")
+cor_inp = input("Por último digite a cor dele:")
+
+carro = Carro(marca_inp, ano_inp, cor_inp)
+
+print(f"\n marca:{carro.marca} \n ano:{carro.ano} \n cor:{carro.cor}")
+        
